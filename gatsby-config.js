0d8a588b3a502d8edaa9d327ec3746ac72367209
Eman <emmanuelgatwech@gmail.com>
2020-01-03 01:28:21 +0300

    added custom styles and fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,14 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Dosis`, `Audiowide`],
+        display: "swap",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
