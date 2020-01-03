83c0d44283f45df1d7bb26aa077626dcf8e2edf1
Eman <emmanuelgatwech@gmail.com>
2020-01-03 03:03:15 +0300

    minor changes

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -187,29 +187,6 @@ const projects = [
   },
 ]
 
-const shuffle = array => {
-  var currentIndex = array.length
-
-  var temporaryValue
-
-  var randomIndex
-
-  // While there remain elements to shuffle...
-  while (currentIndex !== 0) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex)
-    currentIndex -= 1
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex]
-    array[currentIndex] = array[randomIndex]
-    array[randomIndex] = temporaryValue
-  }
-
-  return array
-}
-
-
 // an extra challenge, retrieve all of your repositories from the Github API
 // extract their tags, descriptions and URLs
 // use that instead
